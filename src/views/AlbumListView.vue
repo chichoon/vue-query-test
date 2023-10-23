9849de9f9cfd814aff71f8a62bf310e25bd85a54
chichoon <chichoon.choi@gmail.com>
2023-10-23 14:19:18 +0900

    feat: :sparkles: 앨범 리스트 뷰 추가

--- src/views/AlbumListView.vue
+++ src/views/AlbumListView.vue
@@ -1,11 +1,44 @@
 <template>
-  <div><span>테스트</span></div>
-  <PaginationComponent v-model:currentPage="currentPage" />
+  <LoadingComponent v-if="isLoading" />
+  <ErrorComponent v-else-if="isError" :error="error" />
+  <div v-else-if="!!album" class="album-wrapper">
+    <span>{{ album[currentPage].title }}</span>
+    <img :src="album[currentPage].url" :alt="album[currentPage].title" />
+    <PaginationComponent v-model:currentPage="currentPage" />
+  </div>
+  <PaginationComponent v-model:currentPage="currentAlbum" />
 </template>
 
 <script setup lang="ts">
-import PaginationComponent from '@/components/PaginationComponent.vue';
 import { ref } from 'vue';
 
+import PaginationComponent from '@/components/PaginationComponent.vue';
+import { useGetPhotosByAlbumId } from '@/hooks/photo/useGetPhotoByAlbumId';
+import LoadingComponent from '@/components/LoadingComponent.vue';
+import ErrorComponent from '@/components/ErrorComponent.vue';
+
+const currentAlbum = ref(1);
 const currentPage = ref(1);
+const { data: album, isLoading, isError, error } = useGetPhotosByAlbumId(currentAlbum);
 </script>
+
+<style scoped>
+.album-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  margin-bottom: 20px;
+  border-bottom: 1px solid #ccc;
+
+  & span {
+    font-size: 20px;
+    margin-bottom: 20px;
+  }
+
+  & img {
+    width: 300px;
+    height: 300px;
+  }
+}
+</style>
