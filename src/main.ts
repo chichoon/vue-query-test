249d7863e358a86a268d5a27e72f191f50b82d27
chichoon <chichoon.choi@gmail.com>
2023-10-23 11:02:26 +0900

    feat: :lipstick: reset.css 적용

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,14 @@
+import { VueQueryPlugin } from '@tanstack/vue-query';
 import { createApp } from 'vue';
+
+import '@/assets/base.css';
+
 import App from './App.vue';
 import router from './router';
 
 const app = createApp(App);
 
 app.use(router);
+app.use(VueQueryPlugin);
 
 app.mount('#app');
