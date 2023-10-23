90a99039330f7fd77764376e8e69fffa3be68a95
chichoon <chichoon.choi@gmail.com>
2023-10-23 11:18:53 +0900

    refactor: :label: useGetProfile Typing

--- src/hooks/profile/useGetProfile.ts
+++ src/hooks/profile/useGetProfile.ts
@@ -3,9 +3,10 @@ import { useQuery } from '@tanstack/vue-query';
 import { profileKey } from './queries';
 import profile from '../../services/profile';
 import { CACHE_TIME, STALE_TIME } from '../../utils/constants';
+import { type Profile } from '@/types/profile.d';
 
 export function useGetProfile(id: string) {
-  return useQuery({
+  return useQuery<Profile>({
     queryKey: profileKey.id(id),
     queryFn: () => profile.get(id),
     cacheTime: CACHE_TIME,
