2c98453e3d9ae2866234fcb3be847055563c96b2
chichoon <chichoon.choi@gmail.com>
2023-10-23 10:28:35 +0900

    feat: :sparkles: 프로필 목록 및 프로필 뷰 추가

---
+++ src/views/ProfileListView.vue
@@ -0,0 +1,11 @@
+<template>
+  <ul>
+    <li>hihi</li>
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { useGetProfiles } from '../hooks/profile';
+
+const { data: profiles } = useGetProfiles();
+</script>
---
+++ src/views/ProfileView.vue
@@ -0,0 +1,3 @@
+<template>
+  <span>hihi</span>
+</template>
