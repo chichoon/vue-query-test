b270230b6890a39e60e777f0c0c42d50a6645d6a
chichoon <chichoon.choi@gmail.com>
2023-10-23 10:46:13 +0900

    feat: :zap: useGetProfiles typing

--- src/hooks/profile/useGetProfiles.ts
+++ src/hooks/profile/useGetProfiles.ts
@@ -1,11 +1,12 @@
 import { useQuery } from '@tanstack/vue-query';
 
+import { type Profile } from '@/types/profile';
+import profile from '@/services/profile';
+import { CACHE_TIME, STALE_TIME } from '@/utils/constants';
 import { profileKey } from './queries';
-import profile from '../../services/profile';
-import { CACHE_TIME, STALE_TIME } from '../../utils/constants';
 
 export function useGetProfiles() {
-  return useQuery({
+  return useQuery<Profile[]>({
     queryKey: profileKey.all,
     queryFn: () => profile.getAll(),
     cacheTime: CACHE_TIME,
