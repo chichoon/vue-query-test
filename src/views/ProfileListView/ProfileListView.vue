a061ac3dfd826356567862a146ae7aa6d1bfc4d2
chichoon <chichoon.choi@gmail.com>
2023-10-23 10:57:32 +0900

    feat: :sparkles: 프로필 리스트 추가

---
+++ src/views/ProfileListView/ProfileListView.vue
@@ -0,0 +1,24 @@
+<template>
+  <ul class="list-wrapper">
+    <ProfilePreview v-for="profile in profiles" :key="profile.id" :profile="profile" />
+  </ul>
+</template>
+
+<script setup lang="ts">
+import { useGetProfiles } from '@/hooks/profile';
+import ProfilePreview from './ProfilePreview.vue';
+
+const { data: profiles } = useGetProfiles();
+</script>
+
+<style scoped>
+.list-wrapper {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+  display: grid;
+  grid-template-columns: repeat(2, 1fr);
+  grid-template-rows: repeat(5, 1fr);
+  gap: 5px;
+}
+</style>
---
+++ src/views/ProfileListView/ProfilePreview.vue
@@ -0,0 +1,23 @@
+<template>
+  <li class="element-wrapper">
+    <h3>{{ profile.name }}</h3>
+    <span>{{ profile.email }}</span>
+  </li>
+</template>
+
+<script setup lang="ts">
+import type { Profile } from '@/types/profile';
+
+interface Props {
+  profile: Profile;
+}
+
+const { profile } = defineProps<Props>();
+</script>
+
+<style scoped>
+.element-wrapper {
+  border: 2px solid #12345678;
+  border-radius: 5px;
+}
+</style>
