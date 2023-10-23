bfe567d5b30fc575a8a707123c6eda9c9b3c87e3
chichoon <chichoon.choi@gmail.com>
2023-10-23 09:39:27 +0900

    chore: :bento: 아이콘 정리

---
+++ src/components/PaginationComponent.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <button>
+      {{ `<` }}
+    </button>
+    <button>
+      <span>{{ props.currentPage }}</span>
+    </button>
+    <button>
+      {{ `>` }}
+    </button>
+  </div>
+</template>
+
+<script setup lang="ts">
+interface Props {
+  currentPage: number;
+}
+
+const props = defineProps<Props>();
+</script>
