95671b7ab6a141aa4b87c050384f01fd5516d978
chichoon <chichoon.choi@gmail.com>
2023-10-25 10:53:41 +0900

    deploy

---
+++ assets/constants-2e8668bd.js
@@ -0,0 +1,1 @@
+import{u,Q as t}from"./ErrorComponent.vue_vue_type_script_setup_true_lang-df1bdd39.js";function n(e,s,r){return u(t,e,s,r)}const c=1e3*60*60*24,E=1e3*60*5;export{c as C,E as S,n as u};
